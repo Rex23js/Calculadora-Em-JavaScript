fb7fbfab301651bd30129f9f0287d2f92a21c48e
Rex23js <ismael.b.gomes@aluno.senai.br>
2025-04-20 19:53:14 -0300

    Calculadora Web

--- Calculadora_JavaScript.js
+++ Calculadora_JavaScript.js
@@ -1,34 +1,16 @@
-var teclado = require("prompt-sync")();
-console.log("***CALCULADORA***");
-let Numero1 = teclado("Digite o operador:");
-let Numero2 = teclado("Digite o denominador:");
-let Operacao = teclado("Digite a operação:");
+function num(valor) {
+    document.getElementById("Tela").innerHTML += valor;
+}
 
-switch (Operacao) {
-    case "+":
-        Soma(Numero1, Numero2);
-        break;
-    case "-":
-        Subtracao(Numero1, Numero2);
-        break;
-    case "*":
-        Multiplicacao(Numero1, Numero2);
-        break;
-    case "/":
-        Divisao(Numero1, Numero2);
-        break;
-    default:
-        console.log("operação inválida");
+function LimparTela13() {
+    document.getElementById("Tela").innerHTML = "";
 }
-function Soma(Num1, Num2) {
-    console.log(parseFloat(Num1) + parseFloat(Num2));
-}
-function Subtracao(Num1, Num2) {
-    console.log(parseFloat(Num1) - parseFloat(Num2));
-}
-function Multiplicacao(Num1, Num2) {
-    console.log(parseFloat(Num1) * parseFloat(Num2));
-}
-function Divisao(Num1, Num2) {
-    console.log(parseFloat(Num1) / parseFloat(Num2));
+
+function calcular() {
+    try {
+        let expressao = document.getElementById("Tela").innerHTML;
+        document.getElementById("Tela").innerHTML = eval(expressao);
+    } catch {
+        document.getElementById("Tela").innerHTML = "Erro";
+    }
 }
